8e8fc57be1b06021f134a74e7b99fb595fcb2ae5
Ali Albaharnah <aalbaharnah1@gmail.com>
2019-01-07 16:14:41 +0300

    first commmit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+var app = angular.module("MyWeatherApp", []);
+
+app.controller("MyWeatherController", function ($scope, $http) {
+    $scope.cityName = '';
+    $scope.search = function () {
+        $http.get('http://api.apixu.com/v1/current.json?key=965da890aeb6423f93781514190701&q=' + $scope.cityName).then(function(response){
+        console.log(response.data);    
+        $scope.cityData = response.data.location.name;
+            $scope.cityTemp = response.data.current.temp_c;
+        });
+    }
+});
