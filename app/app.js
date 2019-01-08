168d858dae2177e89fd3707597170d6398334b2d
Ali Albaharnah <aalbaharnah1@gmail.com>
2019-01-08 16:18:48 +0300

    second day commit

--- app/app.js
+++ app/app.js
@@ -6,7 +6,9 @@ app.controller("MyWeatherController", function ($scope, $http) {
         $http.get('http://api.apixu.com/v1/current.json?key=965da890aeb6423f93781514190701&q=' + $scope.cityName).then(function(response){
         console.log(response.data);    
         $scope.cityData = response.data.location.name;
-            $scope.cityTemp = response.data.current.temp_c;
+        $scope.cityTemp = response.data.current.temp_c + '°';
+        $scope.cityWind = response.data.current.wind_kph +' kph';
+        $scope.cityHumidity = response.data.current.humidity + '%';
         });
     }
 });
